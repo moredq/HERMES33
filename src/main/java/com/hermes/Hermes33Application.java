d24aabb1f32abc7b5fed39d7aea9d46777d31b36
moredq <kolyshev-maks@mail.ru>
2026-04-18 17:18:28 +0300

    commit1

--- src/main/java/com/hermes/Hermes33Application.java
+++ src/main/java/com/hermes/Hermes33Application.java
@@ -2,22 +2,10 @@ package com.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Hermes33Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Hermes33Application.class, args);
-    }
-}
-
-// ЭТОТ КОД ДЛЯ ПРОВЕРКИ ↓↓↓
-@RestController
-class TestController {
-    @GetMapping("/")
-    public String hello() {
-        return "HERMES is running!";
     }
 }
